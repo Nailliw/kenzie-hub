e067ff4255b97d5c437ea32a7b46092c92959a94
Jefte Keller <kellergnr@gmail.com>
2020-12-11 14:44:34 -0300

    feat: Navigation bar with fixed imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import NavBarMobile from './components/NavBarMobile';
-import NavBarDesktop from './components/NavBarDesktop';
-import Routes from './routes/index';
-import { useState } from 'react';
-import { CssBaseline, Typography, Container } from '@material-ui/core';
-const App = () => {
-  //const [screenWidth, setScreenWidth] = useState(window.screen.width);
+import "./App.css";
+import { CssBaseline, Typography, Container } from "@material-ui/core";
 
-  return (
-    <>
-      <CssBaseline />
-      <Container maxWidth="xl">
-        <Typography
-          component="div"
-          style={{ backgroundColor: '#cfe8fc', height: '100vh' }}
-        >
-          {window.screen.width > 1024 ? <NavBarDesktop /> : <NavBarMobile />}
-          <Routes />
-        </Typography>
-      </Container>
-    </>
-  );
+import NavBarMobile from "./components/NavBarMobile";
+import NavBarDesktop from "./components/NavBarDesktop";
+
+import Routes from "./Routes";
+
+const App = () => {
+	return (
+		<>
+			<CssBaseline />
+			<Container maxWidth="xl">
+				<Typography
+					component="div"
+					style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+				>
+					{window.screen.width > 1024 ? <NavBarDesktop /> : <NavBarMobile />}
+					<Routes />
+				</Typography>
+			</Container>
+		</>
+	);
 };
 
 export default App;
