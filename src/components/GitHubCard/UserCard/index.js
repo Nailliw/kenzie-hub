d2509d913e4c78622a41d4dc09cb51e54b7f9fab
Willian Ribeiro <willribs29@gmail.com>
2020-12-14 19:10:25 -0300

    chore: fix card bugs and external links

--- src/components/GitHubCard/UserCard/index.js
+++ src/components/GitHubCard/UserCard/index.js
@@ -1,27 +1,45 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Grid } from "@material-ui/core";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardActions,
+  IconButton,
+  Link,
+} from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 const useStyles = makeStyles((theme) => ({
-  root: {},
-  paper: {
-    padding: theme.spacing(2),
-    width: 400,
-    height: 400,
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+  root: {
+    maxWidth: 345,
+    margin: 10,
   },
 }));
-const UserCard = () => {
+const UserCard = ({ list }) => {
   const classes = useStyles();
   return (
     <>
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs>
-            <Paper className={classes.paper}>xs</Paper>
-          </Grid>
-        </Grid>
-      </div>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={<Avatar src={list.avatar_url}></Avatar>}
+          title={list.name}
+          subheader={list.bio}
+        />
+        <CardActions>
+          <IconButton aria-label="Repositório GitHub">
+            <Link href={list.html_url}>
+              <GitHubIcon />
+            </Link>
+          </IconButton>
+          <IconButton aria-label="LinkedIn">
+            <Link href={list.blog}>
+              <LinkedInIcon />
+            </Link>
+          </IconButton>
+        </CardActions>
+      </Card>
     </>
   );
 };
