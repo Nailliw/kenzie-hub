0ed9cf2bc9700dfa6015cf6ec6bbe2f478666b43
Felipe Sobrinho Molina <felipe16sm@gmail.com>
2020-12-17 10:28:24 -0300

    fix: fix profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -8,43 +8,48 @@ import { IsLogged } from "../../components/IsLogged";
 import IsValidState from "../../components/IsValidState";
 
 import ProfileCard from "../../components/Profile/ProfileCard";
+import { updateLoggedUserThunk } from "../../store/modules/loggedUser/thunk";
 
 const Profile = () => {
-	const loggedUser = useSelector((state) => state.LoggedUserReducer);
-	const selectedUser = useSelector((state) => state.SelectedUserReducer);
+  const loggedUser = useSelector((state) => state.LoggedUserReducer);
+  const selectedUser = useSelector((state) => state.SelectedUserReducer);
 
-	const dispatch = useDispatch();
-	const history = useHistory();
-	const location = useLocation();
-	const { userID } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const { userID } = useParams();
 
-	useEffect(() => {
-		if (
-			!IsLogged(dispatch) &&
-			(location.pathname === "/users/profile" ||
-				location.pathname === "/users/edit")
-		) {
-			history.push("/users");
-		}
-	}, [loggedUser.token]);
+  useEffect(() => {
+    dispatch(updateLoggedUserThunk());
+  }, []);
 
-	useEffect(() => {
-		if (userID !== "profile") {
-			dispatch(selectUserThunk(userID));
-		}
-	}, [userID]);
+  useEffect(() => {
+    if (
+      !IsLogged(dispatch) &&
+      (location.pathname === "/users/profile" ||
+        location.pathname === "/users/edit")
+    ) {
+      history.push("/users");
+    }
+  }, [loggedUser.token]);
 
-	return (
-		<>
-			{userID !== "profile"
-				? IsValidState(selectedUser) && (
-						<ProfileCard data={selectedUser} selectedUser={true} />
-				  )
-				: IsValidState(loggedUser.user) && (
-						<ProfileCard data={loggedUser.user} selectedUser={false} />
-				  )}
-		</>
-	);
+  useEffect(() => {
+    if (userID !== "profile") {
+      dispatch(selectUserThunk(userID));
+    }
+  }, [userID]);
+
+  return (
+    <>
+      {userID !== "profile"
+        ? IsValidState(selectedUser) && (
+            <ProfileCard data={selectedUser} selectedUser={true} />
+          )
+        : IsValidState(loggedUser.user) && (
+            <ProfileCard data={loggedUser.user} selectedUser={false} />
+          )}
+    </>
+  );
 };
 
 export default Profile;
